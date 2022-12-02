78073ed25bbee21a18f32ad04351afb36c9b4829
Artemiy <80065244+nkoorty@users.noreply.github.com>
2022-12-02 13:01:41 +0000

    optimised day 2

--- day2.py
+++ day2.py
@@ -1,74 +1,26 @@
 data = open("./AOC2022/data/day2data.txt", "r").read().split('\n')
 
-"""
-Part 1
+total = 0
 
-for line in data:
-    if line[0] == "A":
-        if line[-1] == "X":
-            sum += 1
-            sum += 3
-        elif line[-1] == "Y":
-            sum += 2
-            sum += 6
-        elif line[-1] == "Z":
-            sum += 3
-
-    elif line[0] == "B":
-        if line[-1] == "X":
-            sum += 1
-        elif line[-1] == "Y":
-            sum += 2
-            sum += 3
-        elif line[-1] == "Z":
-            sum += 3
-            sum += 6
-
-    elif line[0] == "C":
-        if line[-1] == "X":
-            sum += 1
-            sum += 6
-        elif line[-1] == "Y":
-            sum += 2
-        elif line[-1] == "Z":
-            sum += 3
-            sum += 3
-"""
-# Part 3
-
-sum = 0
-
-for line in data:
-    if line[0] == "A":
-        if line[-1] == "X":
-            sum += 3
-        elif line[-1] == "Y":
-            sum += 1
-            sum += 3
-        elif line[-1] == "Z":
-            sum += 2
-            sum += 6
-
-    elif line[0] == "B":
-        if line[-1] == "X":
-            sum += 1
-        elif line[-1] == "Y":
-            sum += 2
-            sum += 3
-        elif line[-1] == "Z":
-            sum += 3
-            sum += 6
-
-    elif line[0] == "C":
-        if line[-1] == "X":
-            sum += 2
-        elif line[-1] == "Y":
-            sum += 3
-            sum += 3
-        elif line[-1] == "Z":
-            sum += 1
-            sum += 6
+outcomes = {
+    "A X":4, "A Y":8, "A Z":3,
+    "B X":1, "B Y":5, "B Z":9,
+    "C X":7, "C Y":2, "C Z":6
+}
         
-print(sum)
+for line in data:
+    total += outcomes[line]
 
-sum = 0
+total2 = 0
+
+outcomes2 = {
+    "A X":3, "A Y":4, "A Z":8,
+    "B X":1, "B Y":5, "B Z":9,
+    "C X":2, "C Y":6, "C Z":7
+}    
+
+for line in data:
+    total2 += outcomes2[line]
+
+print(f"The total for Part 1 is: {total}")
+print(f"The total for Part 2 is: {total2}")
