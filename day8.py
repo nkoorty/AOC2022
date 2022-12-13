c62a8b415a6cb362df24cf5d2f305c398e55afca
Artemiy <80065244+nkoorty@users.noreply.github.com>
2022-12-13 03:16:03 +0000

    modified

--- day8.py
+++ day8.py
@@ -1,43 +1,79 @@
-data = open("AOC2022/data/day8data.txt", "r").read().split('\n')
+import numpy as np
+
+data = open("data/day8data.txt", "r").read()
+ 
+def get_score_for_direction(trees, tree_height):
+    if all(tree_height > tree for tree in trees):
+        return len(trees)
+    for index, value in enumerate(trees):
+        if value >= tree_height:
+            return index + 1
+ 
+ 
+def part_one(data):
+    lines_list = data.split('\n')
+    forest_height = len(lines_list)
+    forest_width = len(lines_list[0])
+    forest = np.array(list(data.replace("\n", "")), 'uint8').reshape(forest_height, forest_width)
+ 
+    tree_height = -1
+    trees_visible = 2 * forest_height + 2 * forest_width - 4
+
+    for row in range(1, forest_height-1):
+        for col in range(1, forest_width-1):
+            tree_height = forest[row][col]
+            up = forest[:, col][0:row]
+        
+            if tree_height > np.amax(up):
+                trees_visible += 1
+                continue
+            down = forest[:, col][row:][1:]
+        
+            if tree_height > np.amax(down):
+                trees_visible += 1
+                continue
+            left = forest[row][0:col]
+    
+            if tree_height > np.amax(left):
+                trees_visible += 1
+                continue
+            right = forest[row][col:][1:]
+           
+            if tree_height > np.amax(right):
+                trees_visible += 1
+                continue
+
+    return trees_visible
+
+def part_two(data):
+    lines_list = data.split('\n')
+    forest_height = len(lines_list)
+    forest_width = len(lines_list[0])
+    forest = np.array(list(data.replace("\n", "")), 'uint8').reshape(forest_height, forest_width)
+
+    max_score = -1
+
+    for row in range(0, forest_height):
+        for col in range(0, forest_width):
+            tree_height = forest[row][col]
+            score = 1
+
+            up = np.flip(forest[:, col][0:row])
+            score *= get_score_for_direction(up, tree_height)
+
+            down = forest[:, col][row:][1:]
+            score *= get_score_for_direction(down, tree_height)
+
+            left = np.flip(forest[row][0:col])
+            score *= get_score_for_direction(left, tree_height)
+
+            right = forest[row][col:][1:]
+            score *= get_score_for_direction(right, tree_height)
+
+            max_score = max(score, max_score)
+    
+    return max_score
 
 
-def checker(i, j, data):
-    main = data[i][j]
-    dir_count = 0
-    # Check top
-    for k in range(i):
-        if data[i][k] >= main:
-            break
-        dir_count += 1
-    # Check left 
-    for k in range(j):
-        if data[k][j] >= main:
-            break
-        dir_count += 1
-    # Check right
-    for k in range(len(data[0])-1, i, -1):
-        if data[k][j] >= main:
-            break
-        dir_count += 1
-    # Check bottom
-    for k in range(j, len(data)):
-        if data[i][k] >= main:
-            break
-        dir_count += 1
-
-    if dir_count < 4:
-        return True
-    else:
-        return False
-
-
-def part_1(data):
-    sum = 2*len(data)+ (2*len(data[0])-4)
-    for i in range(1, len(data[0])-1):
-        for j in range(1, len(data)-1):
-            if checker(i,j,data) == True:
-                sum += 1
-    return sum
-
-
-print(part_1(data))
+print(part_one(data))
+print(part_two(data))
